d388815e2539c5b183e7f6e8bcdc22b3152018b9
Albert Sharkis <sharkis@sharkis.org>
2021-03-15 10:35:23 -0600

    linting

--- ardupet/ardupet.ino
+++ ardupet/ardupet.ino
@@ -2,13 +2,13 @@
 Arduboy2 arduboy;
 
 void setup() {
-arduboy.begin();
-arduboy.clear();
+  arduboy.begin();
+  arduboy.clear();
 }
 
 void loop() {
-arduboy.clear();
-arduboy.setCursor(0,0);
-arduboy.print("ardupet");
-arduboy.display();
+  arduboy.clear();
+  arduboy.setCursor(0, 0);
+  arduboy.print("ardupet");
+  arduboy.display();
 }
